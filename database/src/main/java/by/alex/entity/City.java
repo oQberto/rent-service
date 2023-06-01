844f5d6c6d523d8485a416fdddf5802be9b9d41f
Ermakov Alexandr <ermakov.sascha2015yandex.ru@gmail.com>
2023-06-01 18:03:44 +0300

    feat: add City entity

---
+++ database/src/main/java/by/alex/entity/City.java
@@ -0,0 +1,15 @@
+package by.alex.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class City {
+    private Integer id;
+    private String name;
+}
