d3dac105f94f1a7f7ae4dd196d6a754e3ff1b3eb
Ermakov Alexandr <ermakov.sascha2015yandex.ru@gmail.com>
2023-06-02 12:55:27 +0300

    feat: add CityService.java and realize findAll() method

---
+++ service/src/main/java/by/alex/service/CityService.java
@@ -0,0 +1,27 @@
+package by.alex.service;
+
+import by.alex.dao.CityDao;
+import by.alex.dto.CityDto;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+import static lombok.AccessLevel.PRIVATE;
+
+@NoArgsConstructor(access = PRIVATE)
+public class CityService {
+    private static final CityService INSTANCE = new CityService();
+    private final CityDao cityDao = CityDao.getInstance();
+
+    public List<CityDto> findAll() {
+        return cityDao.findAll().stream()
+                .map(city -> CityDto.builder()
+                        .name(city.getName())
+                        .build())
+                .toList();
+    }
+
+    public static CityService getInstance() {
+        return INSTANCE;
+    }
+}
