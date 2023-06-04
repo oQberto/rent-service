f15b62e73980038cadd6d6cb91219c13dfca69b0
Ermakov Alexandr <ermakov.sascha2015yandex.ru@gmail.com>
2023-06-04 20:49:21 +0300

    feat: realize findByApartmentId(Integer id) method

--- database/src/main/java/by/alex/dao/ApartmentPhotoDao.java
+++ database/src/main/java/by/alex/dao/ApartmentPhotoDao.java
@@ -1,8 +1,16 @@
 package by.alex.dao;
 
+import by.alex.entity.Apartment;
 import by.alex.entity.ApartmentPhoto;
+import by.alex.util.ConnectionManager;
 import lombok.NoArgsConstructor;
+import lombok.SneakyThrows;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -10,7 +18,29 @@ import static lombok.AccessLevel.PRIVATE;
 
 @NoArgsConstructor(access = PRIVATE)
 public class ApartmentPhotoDao implements Dao<Integer, ApartmentPhoto> {
+    private static final ApartmentDao APARTMENT_DAO = ApartmentDao.getInstance();
     private static final ApartmentPhotoDao INSTANCE = new ApartmentPhotoDao();
+    private static final String FIND_BY_APARTMENT_ID_SQL = """
+            SELECT *
+            FROM apartment_photo
+            WHERE apartment_id = ?
+            """;
+
+    @SneakyThrows
+    public List<ApartmentPhoto> findByApartmentId(Integer id) {
+        List<ApartmentPhoto> photos = new ArrayList<>();
+        try (Connection connection = ConnectionManager.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(FIND_BY_APARTMENT_ID_SQL)) {
+
+            preparedStatement.setInt(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                photos.add(build(resultSet));
+            }
+            return photos;
+        }
+    }
 
     @Override
     public List<ApartmentPhoto> findAll() {
@@ -35,6 +65,16 @@ public class ApartmentPhotoDao implements Dao<Integer, ApartmentPhoto> {
     @Override
     public boolean delete(ApartmentPhoto entity) {
         return false;
+    }
+
+    private ApartmentPhoto build(ResultSet resultSet) throws SQLException {
+        return ApartmentPhoto.builder()
+                .id(resultSet.getInt("id"))
+                .photoPath(resultSet.getString("photo_path"))
+                .apartment(APARTMENT_DAO
+                        .findById(resultSet.getInt("apartment_id"))
+                        .orElse(new Apartment()))
+                .build();
     }
 
     public static ApartmentPhotoDao getInstance() {
