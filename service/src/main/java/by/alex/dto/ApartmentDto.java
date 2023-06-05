db6bad290d4802d2b4c8cf433243d5d73980b220
Ermakov Alexandr <ermakov.sascha2015yandex.ru@gmail.com>
2023-06-05 13:10:46 +0300

    fix: fix ApartmentDto.java

--- service/src/main/java/by/alex/dto/ApartmentDto.java
+++ service/src/main/java/by/alex/dto/ApartmentDto.java
@@ -1,23 +1,19 @@
 package by.alex.dto;
 
 import by.alex.entity.Address;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.EqualsAndHashCode;
 import lombok.Value;
 
 import java.time.LocalDateTime;
 
-@EqualsAndHashCode(callSuper = true)
 @Value
 @Builder
-@AllArgsConstructor
-public record ApartmentDto(
-        String id,
-        String propertyType,
-        LocalDateTime yearBuilt,
-        Boolean petFriendly,
-        Boolean furnished,
-        String leaseTerm,
-        Address address) {
+public class ApartmentDto {
+        Integer id;
+        String propertyType;
+        LocalDateTime yearBuilt;
+        Boolean petFriendly;
+        Boolean furnished;
+        String leaseTerm;
+        Address address;
 }
