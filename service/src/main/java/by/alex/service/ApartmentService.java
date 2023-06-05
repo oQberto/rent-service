94c30c710fb37439d89c94dd54d9938511bd32f5
Ermakov Alexandr <ermakov.sascha2015yandex.ru@gmail.com>
2023-06-05 13:10:29 +0300

    feat: create ApartmentService.java

---
+++ service/src/main/java/by/alex/service/ApartmentService.java
@@ -0,0 +1,33 @@
+package by.alex.service;
+
+import by.alex.dao.ApartmentDao;
+import by.alex.dto.ApartmentDto;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+import static lombok.AccessLevel.PRIVATE;
+
+@NoArgsConstructor(access = PRIVATE)
+public class ApartmentService {
+    private static final ApartmentService INSTANCE = new ApartmentService();
+    private final ApartmentDao apartmentDao = ApartmentDao.getInstance();
+
+    public List<ApartmentDto> findAll() {
+        return apartmentDao.findAll().stream()
+                .map(apartment -> ApartmentDto.builder()
+                        .id(apartment.getId())
+                        .propertyType(apartment.getPropertyType().name())
+                        .yearBuilt(apartment.getYearBuilt())
+                        .petFriendly(apartment.getPetFriendly())
+                        .furnished(apartment.getFurnished())
+                        .leaseTerm(apartment.getLeaseTerm().name())
+                        .address(apartment.getAddress())
+                        .build())
+                .toList();
+    }
+
+    public static ApartmentService getInstance() {
+        return INSTANCE;
+    }
+}
